7ae794c8a125a8db2f59debd9a8d6577122b3c8a
SebastianR0589 <sebastian.riedel0589@gmail.com>
2025-08-28 16:04:27 +0200

    reworked dice value array into object

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import Die from './components/Die.jsx'
 import React from 'react'
-
+import { nanoid } from 'nanoid'
 
 
 
 function App() {
 
-  const [diceState, setDiceState] = React.useState(generateAlllNewDice())
-
-  const diceElements = diceState.map(num => <Die value={num} />)  //“for this number, make a Die component”
+  const [diceState, setDiceState] = React.useState(generateAlllNewDice()) 
+  
+  const diceElements = diceState.map(dieObj => <Die key={dieObj.id} value={dieObj.value} />)  //“for this number, make a Die component”
 
 
   function generateAlllNewDice() {
@@ -17,7 +17,7 @@ function App() {
       for (let i = 0; i < 10; i++) {
         diceArray.push( Math.ceil(Math.random() * 6))
       }
-      return diceArray
+      return diceArray.map(num => ({value: num, isHeld: false, id:nanoid()}))
     }
 
   function rollDice() {
