9a2b9c45d360cc591f6410ab6456934ab3ea9a5f
SebastianR0589 <sebastian.riedel0589@gmail.com>
2025-08-28 15:28:24 +0200

    added random functionality instead of hardcoded numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import './App.css'
 import Die from './components/Die.jsx'
+import React from 'react'
+
+
 
 
 function App() {
-  
+
+  const [diceState, setDiceState] = React.useState(generateAlllNewDice())
+
+  const diceElements = diceState.map(num => <Die value={num} />)  //“for this number, make a Die component”
+
+
+  function generateAlllNewDice() {
+    let diceArray = [] 
+      for (let i = 0; i < 10; i++) {
+        diceArray.push( Math.ceil(Math.random() * 6))
+      }
+      return diceArray
+    }
+
 
   return (
 
     <main>
     <h1>Tenzies</h1>
     <div className='dice-container'>
-     <Die value="1"/>
-     <Die value="1"/>
-     <Die value="1"/>
-     <Die value="1"/>
-     <Die value="1"/>
-     <Die value="1"/>
-     <Die value="1"/>
-     <Die value="1"/>
-     <Die value="1"/>
-     <Die value="1"/>
+     {diceElements}
      </div>
     </main>
 
