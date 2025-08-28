7469fba942e5dbb986eeb20f06159dcd6d260390
SebastianR0589 <sebastian.riedel0589@gmail.com>
2025-08-28 11:57:30 +0200

    Basic code structure added

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import Die from './components/Die.jsx'
+
+
+function App() {
+  
+
+  return (
+
+    <main>
+    <h1>Tenzies</h1>
+    <div className='dice-container'>
+     <Die value="1"/>
+     <Die value="1"/>
+     <Die value="1"/>
+     <Die value="1"/>
+     <Die value="1"/>
+     <Die value="1"/>
+     <Die value="1"/>
+     <Die value="1"/>
+     <Die value="1"/>
+     <Die value="1"/>
+     </div>
+    </main>
+
+  )
+}
+
+export default App
