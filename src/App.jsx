dfda52c212f009d488184e29901089d663d953a6
SebastianR0589 <sebastian.riedel0589@gmail.com>
2025-09-01 16:00:33 +0200

    added win condition and reset functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import Die from './components/Die.jsx'
 import React from 'react'
 import { nanoid } from 'nanoid'
 
+import Confetti from 'react-confetti'
+
 
 
 function App() {
 
-  const [diceState, setDiceState] = React.useState(generateAlllNewDice()) 
+  const [diceState, setDiceState] = React.useState(() => generateAllNewDice()) 
   
   const diceElements = diceState.map(dieObj => <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} holdDice={holdDice} id={dieObj.id}/>)  //“for this number, make a Die component”
 
+  const gameWon =  diceState.every(die => die.isHeld) && diceState.every(die => die.value === diceState[0].value)
 
-  function generateAlllNewDice() {
+
+
+  function generateAllNewDice() {
     let diceArray = [] 
       for (let i = 0; i < 10; i++) {
         diceArray.push( Math.ceil(Math.random() * 6))
@@ -36,15 +41,26 @@ function holdDice(id) {
   )
 }
 
+function handleButtonClick() {
+  if (gameWon) {
+    setDiceState(generateAllNewDice());
+  } else {
+    rollDice();
+  }
+}
+
+
   return (
 
     <main>
     <h1>Tenzies</h1>
+    <p className="instructions">To win all dice need to match. Click the dice with the right number to keep it. Reroll untill you won.</p>
     <div className='dice-container'>
      {diceElements}
      </div>
 
-     <button className="roll-dice-button" onClick={rollDice}>Roll Dice</button>
+     <button className="roll-dice-button" onClick={handleButtonClick}>{gameWon ? "Play Again" : "Roll Dice"}</button>
+      {gameWon && <Confetti/>}
     </main>
 
   )
